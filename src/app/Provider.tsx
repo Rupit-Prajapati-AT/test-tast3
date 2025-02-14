2b0993d2671d25bcc73b1eedfcdf359f384eb738
Rupit Prajapati <113025099+Rupit-Prajapati-AT@users.noreply.github.com>
2025-02-14 14:06:29 +0530

    reverting  changes

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,9 +1,8 @@
 "use client";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-function Provider(props: { children: React.ReactNode }) {
+
+export default function Provider(props: { children: React.ReactNode }) {
   return (
     <ChakraProvider value={defaultSystem}>{props.children}</ChakraProvider>
   );
 }
-
-export { Provider };
